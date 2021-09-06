3cbc368ecb8bcc886e2f9889a6d341f7f36c8853
Brigitte Powers PC <reyhn701@gmail.com>
2021-09-06 12:26:29 -0500

    Added weekly forecast

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 var search = document.querySelector(".search-form");
 var cityInput = document.querySelector(".search-form input");
+var display = document.querySelector(".search-list");
 
 //open weather API
 var apiKey = "95dda93988614202cb796a38de218adc";
@@ -10,6 +11,8 @@ var unitsIcon = "°F"
 // user searches a city by submitting the form
 search.addEventListener("submit", async function(event){
     event.preventDefault();
+
+    display.innerHTML = "";
 
     // record user's city
     city = cityInput.value;
@@ -23,19 +26,25 @@ search.addEventListener("submit", async function(event){
     var cityForecast = await getCityWeather(cityLat, cityLon);
 
     // display data to user
-    dataDisplay(cityForecast);
+    forecastDisplay(cityForecast);
 
     // if the city is not on the cities list, add it
 
 });
 
-function dataDisplay (data) {
+function forecastDisplay (data) {
     //make elements
-    var forecastDisplay = document.querySelector(".search-list");
     var bubbleData = document.createElement("ul");
     bubbleData.classList.add("data-bubble");
     var dailyData = document.createElement("ul");
     dailyData.classList.add("data-daily");
+    var weeklyData = document.createElement("ul");
+    weeklyData.classList.add("data-weekly");
+
+    //calculate time stamps
+    var dayname = new Date(data.daily[0].dt * 1000).toLocaleDateString("en", {
+        weekday: "long",
+    });
 
     //bubbleData list items
     var iconDis = document.createElement("li");
@@ -44,29 +53,71 @@ function dataDisplay (data) {
     var tempDis = document.createElement("li");
 
     //bubble data innerHTML
-    iconDis.innerHTML = "<img src= http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png></img>"; //weather icon
+    iconDis.innerHTML = "<img src= http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + "@2x.png></img>"; //weather icon
     cityDis.innerHTML = city; // city name
-    descDis.innerHTML = data.current.weather[0].description; //desc
-    tempDis.innerHTML = Math.trunc(data.current.temp) + "<sup>" + unitsIcon + "</sup>"; //temperature
+    descDis.innerHTML = data.daily[0].weather[0].description; //desc
+    tempDis.innerHTML = Math.trunc(data.daily[0].temp.max) + "<sup>" + unitsIcon + "</sup> - " + Math.trunc(data.daily[0].temp.min) + "<sup>" + unitsIcon + "</sup>"; //temperature
 
     //bubble data append
-    forecastDisplay.appendChild(bubbleData);
+    display.appendChild(bubbleData);
     bubbleData.appendChild(iconDis);
     bubbleData.appendChild(cityDis);
     bubbleData.appendChild(descDis);
     bubbleData.appendChild(tempDis);
 
     // daily list items
+    var dateDis = document.createElement("li");
+    var humidDis = document.createElement("li");
+    var windDis = document.createElement("li");
+    var uvDis = document.createElement("li");
 
     // daily data innerHTML
-        //date
-        //humidity
-        //wind speed
-        //UV index - color by severity
+        dateDis.innerHTML = dayname; //date
+        humidDis.innerHTML = "Humidity: " + data.daily[0].humidity + "%";//humidity
+        windDis.innerHTML = "Wind: " + data.daily[0].wind_speed + " mph"//wind speed
+        uvDis.innerHTML = "UV Index: " + data.daily[0].uvi + " - " //UV index //TODO: color uvi by severity
 
     // daily data append
-    forecastDisplay.appendChild(dailyData);
+    display.appendChild(dailyData);
+    dailyData.appendChild(dateDis);
+    dailyData.appendChild(humidDis);
+    dailyData.appendChild(windDis);
+    dailyData.appendChild(uvDis);
 
+    // 4 days out
+    for (i = 1; i < 5; i++) {
+        // create elements
+
+        var day = new Date(data.daily[i].dt * 1000).toLocaleDateString("en", {
+            weekday: "long",
+        });
+
+        var iconDisWeek = document.createElement("li");
+        var descDisWeek = document.createElement("li");
+        var tempDisWeek = document.createElement("li");
+        var dateDisWeek = document.createElement("li");
+        var humidDisWeek = document.createElement("li");
+        var windDisWeek = document.createElement("li");
+        var uvDisWeek = document.createElement("li");
+
+        iconDisWeek.innerHTML = "<img src= http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png></img>"; //weather icon
+        descDisWeek.innerHTML = data.daily[i].weather[0].description; //desc
+        tempDisWeek.innerHTML = Math.trunc(data.daily[i].temp.max) + "<sup>" + unitsIcon + "</sup> - " + Math.trunc(data.daily[0].temp.min) + "<sup>" + unitsIcon + "</sup>"; //temperature
+        dateDisWeek.innerHTML = day; //date
+        humidDisWeek.innerHTML = "Humidity: " + data.daily[i].humidity + "%";//humidity
+        windDisWeek.innerHTML = "Wind: " + data.daily[i].wind_speed + " mph"//wind speed
+        uvDisWeek.innerHTML = "UV Index: " + data.daily[i].uvi + " - " //UV index //TODO: color uvi by severity
+
+        weeklyData.appendChild(iconDisWeek);
+        weeklyData.appendChild(dateDisWeek);
+        weeklyData.appendChild(descDisWeek);
+        weeklyData.appendChild(tempDisWeek);
+        weeklyData.appendChild(humidDisWeek);
+        weeklyData.appendChild(windDisWeek);
+        weeklyData.appendChild(uvDisWeek);
+
+        display.appendChild(weeklyData);
+    }
 
 }
 
@@ -94,8 +145,6 @@ function getCityWeather(lat, lon) {
             return data;
         })
 }
-
-// User presented with current and future conditions for that city
 
 // City is added to search history - local storage
 
